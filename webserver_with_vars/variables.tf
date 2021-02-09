158356e721c2402263617a9e469ac591a2cb41fa
Ndomi <n.nqeketo@yahoo.co.za>
2021-02-09 22:07:37 +0200

    Using variables

---
+++ webserver_with_vars/variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "http_port" {
+  default = "http_port"
+}
+
+variable "ssh_port" {
+  default = "ssh_port"
+}
+
+variable "ip_address" {
+  default = "xxx.xxx.xxx.xxx/32"
+}
+
+variable "ami" {
+  default = "ami-047a51fa27710816e"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "K-name" {
+  default = "New_Key"
+}
