44a5c1185962c13bdd953197d2e8dd1e6e0a7694
Ndomi <n.nqeketo@yahoo.co.za>
2021-02-09 19:05:42 +0200

    Add tags to EC2

---
+++ deploy-single-webserver/security_group.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "web_SG" {
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["102.222.182.45/32"]
+  }
+
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["102.222.182.45/32"]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
