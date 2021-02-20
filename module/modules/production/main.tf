1a2da7ef7f837b712a4ec8abeada361efe19133b
ndomi <n.nqeketo@yahoo.co.za>
2021-02-20 20:58:31 +0200

    Using tag v0.0.2

--- module/modules/production/main.tf
+++ module/modules/production/main.tf
@@ -1,19 +1,11 @@
-/*
 module "users_group_creation" {
-  source = "../Users"
-}
-*/
-
-module "users_group_creation" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.0.1"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.0.2"
 }
 
 module "prod_computing" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.0.1"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.0.2"
 }
 
 module "ec2_monitoring" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.0.1"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.0.2"
 }
-
-### Place holder ###
