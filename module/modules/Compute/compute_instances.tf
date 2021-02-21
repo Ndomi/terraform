604c950e3729b07e74d48164dfc617b9d8e3610d
ndomi <n.nqeketo@yahoo.co.za>
2021-02-21 15:29:27 +0200

    Change to  tag v0.0.7

--- module/modules/Compute/compute_instances.tf
+++ module/modules/Compute/compute_instances.tf
@@ -1,5 +1,5 @@
 module "networking" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.0.6"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.0.7"
 }
 
 resource "aws_instance" "Jumpbox_A" {
