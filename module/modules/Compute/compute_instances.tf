ddca8601f6a69147bc7d886ec02fe2230a1021ca
ndomi <n.nqeketo@yahoo.co.za>
2021-02-22 11:36:30 +0200

    v0.0.11

--- module/modules/Compute/compute_instances.tf
+++ module/modules/Compute/compute_instances.tf
@@ -1,5 +1,5 @@
 module "networking" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.0.10"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.0.11"
 }
 
 resource "aws_instance" "Jumpbox_A" {
--- module/modules/Users/policy_management.tf
+++ module/modules/Users/policy_management.tf
@@ -2,20 +2,19 @@ resource "aws_iam_role" "ec2_role" {
   name = "ec2-role"
   assume_role_policy = <<EOF
 {
-  "Version" = "2012-10-17"
-  "Statement" = [
+  "Version": "2012-10-17",
+  "Statement": [
     {
-      "Action" = "sts:AssumeRole",
-      "Principal" = {
-        "Service" = "ec2.amazonaws.com"
+      "Effect": "Allow",
+      "Sid": "",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
       },
-      "Effect" = "Allow",
-      "Sid"    = ""
+      "Action": "sts:AssumeRole"
     }
   ]
 }
 EOF
-
   tags = {
     Name = "ec2-role"
   }
