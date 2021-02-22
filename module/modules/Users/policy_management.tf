aa8fc5b9cd3fa4f292496f7724f1575bce18ee6f
ndomi <n.nqeketo@yahoo.co.za>
2021-02-22 11:13:39 +0200

    Fixing The EOF Syntax

--- module/modules/Users/policy_management.tf
+++ module/modules/Users/policy_management.tf
@@ -1,19 +1,19 @@
 resource "aws_iam_role" "ec2_role" {
   name = "ec2-role"
   assume_role_policy = <<EOF
-  {
-    "Version" = "2012-10-17"
-    "Statement" = [
-      {
-        "Action" = "sts:AssumeRole",
-        "Principal" = {
-          "Service" = "ec2.amazonaws.com"
-        },
-        "Effect" = "Allow",
-        "Sid"    = ""
-      }
-    ]
-  }
+{
+  "Version" = "2012-10-17"
+  "Statement" = [
+    {
+      "Action" = "sts:AssumeRole",
+      "Principal" = {
+        "Service" = "ec2.amazonaws.com"
+      },
+      "Effect" = "Allow",
+      "Sid"    = ""
+    }
+  ]
+}
 EOF
 
   tags = {
