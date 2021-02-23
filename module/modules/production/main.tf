f904694edd9bf45a3770abb6f9d94203f0418d0c
ndomi <n.nqeketo@yahoo.co.za>
2021-02-23 21:16:39 +0200

    Correcting the variables

--- module/modules/production/main.tf
+++ module/modules/production/main.tf
@@ -1,20 +1,20 @@
 module "users_group_creation" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.0.42"
-  dev = ""
-  devops = ""
-  ops = ""
-  testing = ""
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.0.44"
+  ops = "operations"
+  dev = "development"
+  testing = "testers"
+  devops = "DevOps"
 }
 
 module "prod_computing" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.0.42"
-  availability_zone_A = ""
-  availability_zone_B = ""
-  ec2_ami = ""
-  http_port = ""
-  key = ""
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.0.44"
+  ec2_ami = "ami-047a51fa27710816e"
+  key = "New_Key"
+  http_port = 80
+  availability_zone_B = "us-east-1b"
+  availability_zone_A = "us-east-1a"
 }
 
 module "ec2_monitoring" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.0.42"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.0.44"
 }
--- module/modules/production/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "ec2_ami" {}
-
-variable "key" {}
-
-variable "http_port" {}
-
-variable "availability_zone_B" {}
-
-variable "availability_zone_A" {}
