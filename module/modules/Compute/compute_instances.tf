de5340bae7ef8a4c7294abff022436b66d82e1ab
ndomi <n.nqeketo@yahoo.co.za>
2021-02-23 21:26:15 +0200

    Correcting the variables

--- module/modules/Compute/compute_instances.tf
+++ module/modules/Compute/compute_instances.tf
@@ -1,5 +1,5 @@
 module "networking" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.0.45"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.0.46"
 }
 
 resource "aws_instance" "Jumpbox_A" {
