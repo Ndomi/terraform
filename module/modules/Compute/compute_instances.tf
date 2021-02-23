a9abaffde9b0550320f0970852a9385ab4eb6bd7
ndomi <n.nqeketo@yahoo.co.za>
2021-02-23 10:34:14 +0200

    Fixing workspaces(v0.0.24)

--- module/modules/Compute/compute_instances.tf
+++ module/modules/Compute/compute_instances.tf
@@ -1,5 +1,5 @@
 module "networking" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.0.23"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.0.24"
 }
 
 resource "aws_instance" "Jumpbox_A" {
