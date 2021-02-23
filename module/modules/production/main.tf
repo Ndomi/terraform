396b55d5aa32ed704f9a8c99c588b218af199a0a
ndomi <n.nqeketo@yahoo.co.za>
2021-02-23 09:28:07 +0200

    Fixing workspaces(v0.0.22)

--- module/modules/production/main.tf
+++ module/modules/production/main.tf
@@ -1,11 +1,11 @@
 module "users_group_creation" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.0.21"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.0.22"
 }
 
 module "prod_computing" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.0.21"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.0.22"
 }
 
 module "ec2_monitoring" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.0.21"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.0.22"
 }
