7b617074e927f6bebc1ba8b1fcb27fc7c59ac429
ndomi <n.nqeketo@yahoo.co.za>
2021-02-23 10:17:06 +0200

    Fixing workspaces(v0.0.23)

--- module/modules/Compute/iam_roles.tf
+++ module/modules/Compute/iam_roles.tf
@@ -24,5 +24,5 @@ EOF
 
 resource "aws_iam_instance_profile" "ec2_profile" {
   name      = "ec2_profile"
-  role      = lookup(aws_iam_role.ec2_role.name,terraform.workspace)
+  role_name      = aws_iam_role.ec2_role.name
 }
