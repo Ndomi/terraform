d95a8cef9de5efce3231c8fc4925f077b839cd01
ndomi <n.nqeketo@yahoo.co.za>
2021-02-24 18:52:17 +0200

    Initializing all modules

--- module/modules/Compute/compute_instances.tf
+++ module/modules/Compute/compute_instances.tf
@@ -1,5 +1,5 @@
 module "networking" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.0.44"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.0.45"
 }
 
 resource "aws_instance" "Jumpbox_A" {
