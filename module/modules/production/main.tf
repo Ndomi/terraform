203937f25ad566ddb90c854f14e5605d647f5325
ndomi <n.nqeketo@yahoo.co.za>
2021-02-24 23:05:45 +0200

    Applying v6

--- module/modules/production/main.tf
+++ module/modules/production/main.tf
@@ -1,12 +1,12 @@
 module "users_group_creation" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.0.57"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.0.58"
 }
 
 module "prod_computing" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.0.57"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.0.58"
 
 }
 
 module "ec2_monitoring" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.0.57"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.0.58"
 }
