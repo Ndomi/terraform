c5394ec441674fd8186df918368841d55ac3daea
ndomi <n.nqeketo@yahoo.co.za>
2021-02-24 21:53:02 +0200

    Initialize Compute Module

--- module/modules/production/main.tf
+++ module/modules/production/main.tf
@@ -1,12 +1,12 @@
 module "users_group_creation" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.0.48"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.0.49"
 }
 
 module "prod_computing" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.0.48"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.0.49"
 
 }
 
 module "ec2_monitoring" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.0.48"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.0.49"
 }
