debe05273f406f596f23facb146c1d8ceca75015
ndomi <n.nqeketo@yahoo.co.za>
2021-03-01 10:14:19 +0200

    v0.1.34

--- module/modules/Compute/compute_instances.tf
+++ module/modules/Compute/compute_instances.tf
@@ -1,5 +1,5 @@
 module "networking" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.1.33"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.1.34"
 }
 
 resource "aws_instance" "Jumpbox_A" {
--- module/modules/production/main.tf
+++ module/modules/production/main.tf
@@ -1,12 +1,12 @@
 module "users_group_creation" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.1.33"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.1.34"
 }
 
 module "prod_computing" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.1.33"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.1.34"
 
 }
 
 module "ec2_monitoring" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.1.33"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.1.34"
 }
