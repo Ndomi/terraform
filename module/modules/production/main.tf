ad124a24669f553daf5072155382029797d09349
ndomi <n.nqeketo@yahoo.co.za>
2021-03-02 12:01:07 +0200

    adding public ssh keys

--- module/modules/production/main.tf
+++ module/modules/production/main.tf
@@ -1,12 +1,12 @@
 module "users_group_creation" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.1.40"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.1.42"
 }
 
 module "prod_computing" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.1.40"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.1.42"
 
 }
 
 module "ec2_monitoring" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.1.40"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.1.42"
 }
