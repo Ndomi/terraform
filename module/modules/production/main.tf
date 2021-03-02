ceb2df5ff2df87c5b269ea65fdfa2c7f24cf3337
ndomi <n.nqeketo@yahoo.co.za>
2021-03-03 00:32:37 +0200

    Create S3 Bucket with Ansible

--- module/modules/production/main.tf
+++ module/modules/production/main.tf
@@ -1,12 +1,12 @@
 module "users_group_creation" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.2.6"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Users?ref=v0.2.7"
 }
 
 module "prod_computing" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.2.6"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Compute?ref=v0.2.7"
 
 }
 
 module "ec2_monitoring" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.2.6"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/Logging?ref=v0.2.7"
 }
