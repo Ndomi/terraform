99da795c4143aa59dd3123ed4607fad1bdc1fe0e
ndomi <n.nqeketo@yahoo.co.za>
2021-03-03 00:37:18 +0200

    Create S3 Bucket with Ansible

--- module/modules/Compute/compute_instances.tf
+++ module/modules/Compute/compute_instances.tf
@@ -1,5 +1,5 @@
 module "networking" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.2.7"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.2.8"
 }
 
 resource "aws_instance" "Jumpbox_A" {
