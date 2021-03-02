cb86ccdbbbcd30cfe4e84d824e68331d2b1c747d
ndomi <n.nqeketo@yahoo.co.za>
2021-03-02 16:46:30 +0200

    Fix v2

--- module/modules/Compute/compute_instances.tf
+++ module/modules/Compute/compute_instances.tf
@@ -1,5 +1,5 @@
 module "networking" {
-  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.1.50"
+  source = "git::https://github.com/Ndomi/terraform.git//module/modules/networking?ref=v0.1.51"
 }
 
 resource "aws_instance" "Jumpbox_A" {
