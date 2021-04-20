ad8cdbb9b09a484c92b0b952162e5945fdb91330
Soho <jenkins@sohotech.io>
2021-04-20 13:30:33 +0200

    Add Bastion and Private EC2

--- Security/AWS_Systems_Manager/computing/ec2.tf
+++ Security/AWS_Systems_Manager/computing/ec2.tf
@@ -2,16 +2,29 @@ module "networking" {
   source = "../networking"
 }
 
-resource "aws_instance" "ec2" {
+resource "aws_instance" "bastion-ec2" {
   ami                  = var.ami-ID
   instance_type        = "t2.micro"
-  key_name             = aws_key_pair.ssmkey.key_name
   subnet_id            = module.networking.publicSN_A
   security_groups      = [module.networking.publicSG_A]
   iam_instance_profile = aws_iam_instance_profile.profile.id
+  key_name             = aws_key_pair.ssmkey.key_name
   user_data            = file("${path.module}/install-ssm.sh")
 
   tags = {
-    Name = "EC2 Instance"
+    Name = "Bastion EC2"
+  }
+}
+
+resource "aws_instance" "private-ec2" {
+  ami = var.ami-ID
+  instance_type = "t2.micro"
+  subnet_id = module.networking.privateSN_A
+  security_groups = [module.networking.privateSG_A]
+  key_name = aws_key_pair.ssmkey.key_name
+  user_data = file("${path.module}/install-ssm.sh")
+
+  tags = {
+    Name = "Private EC2"
   }
 }
