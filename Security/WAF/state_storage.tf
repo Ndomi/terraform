11ff13c0f711d6c6f06ef0adecc7986caecae4cc
Soho <jenkins@sohotech.io>
2021-05-10 21:37:13 +0200

    Adding Storage

---
+++ Security/WAF/state_storage.tf
@@ -0,0 +1,40 @@
+/*resource "aws_s3_bucket" "terraform-state-storage-s3" {
+  bucket = "example.ndomi.state-1"
+  versioning {
+    enabled = true
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+  tags = {
+    Name = "s3 Remote Terraform State Stote"
+  }
+}*/
+
+terraform {
+  backend "s3" {
+    encrypt = true
+    bucket  = "example.ndomi.state-1"
+//    dynamodb_table = "dynamodb-terraform-state-lock"
+    region = "us-east-1"
+    key    = "terraform.tfstate"
+
+  }
+}
+
+#####  Enable the below code to prevent multiple user to edit the state ######
+##### Charges apply #####
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  hash_key = "LockID"
+  name = "terraform-state-lock-dynamo"
+  read_capacity = 20
+  write_capacity = 20
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags = {
+    Name = " DynamoDB Terraform State Lock Table"
+  }
+}
